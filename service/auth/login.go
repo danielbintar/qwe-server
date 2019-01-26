a402dc60976aeeb8309cb0063ddfb9b8e5d318d3
@danielbintar <daniel_bintar@yahoo.com>
2019-01-26 22:19:17 +0700

    use hash

--- service/auth/login.go
+++ service/auth/login.go
@@ -1,11 +1,15 @@
 package auth
 
 import (
+	"errors"
+
 	"github.com/danielbintar/qwe-server/model"
 
 	"github.com/danielbintar/go-record/db"
 
 	"gopkg.in/validator.v2"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginForm struct {
@@ -29,10 +33,14 @@ func (self *LoginForm) Validate() []error {
 
 func (self *LoginForm) Perform() (interface{}, []error) {
 	user := &model.User{}
-	err := db.FindBy(&user, []string{"username", "=", self.Username}, []string{"password", "=", self.Password})
+	err := db.FindBy(&user, []string{"username", "=", self.Username})
 
 	if err != nil {
 		return user, []error{err}
+	}
+
+	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(self.Password)); err != nil {
+		return user, []error{errors.New("not found")}
 	}
 
 	return user, nil
