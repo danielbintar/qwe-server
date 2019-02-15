9aed0ab7bf3feabe9ad50df2048c02e6ccc3ec90
@danielbintar <daniel_bintar@yahoo.com>
2019-02-15 21:14:59 +0700

    current character

--- app/http/main.go
+++ app/http/main.go
@@ -20,12 +20,24 @@ func main() {
 	r.Route("/users", func(r chi.Router) {
 		r.Post("/sign_in", controller.Login)
 		r.Post("/sign_up", controller.CreateUser)
+
+		r.Route("/me", func(r chi.Router) {
+			r.Use(controller.Authenticated)
+			r.Use(controller.MyUser)
+			r.Get("/", controller.GetMyUser)
+			r.Get("/current-character", controller.GetMyUserCurrentCharacter)
+		})
 	})
 
 	r.Route("/my-characters", func(r chi.Router) {
 		r.Use(controller.Authenticated)
 		r.Get("/", controller.GetMyCharacters)
 		r.Post("/", controller.CreateMyCharacter)
+
+		r.Route("/{characterID}", func(r chi.Router) {
+			r.Use(controller.MyCharacter)
+			r.Post("/play", controller.PlayMyCharacter)
+		})
 	})
 
 	r.Route("/towns", func(r chi.Router) {
