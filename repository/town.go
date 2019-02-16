7135bd2e26af5211559ca1824ee140fb49e6343f
@danielbintar <daniel_bintar@yahoo.com>
2019-02-16 21:41:10 +0700

    default position

--- repository/town.go
+++ repository/town.go
@@ -32,6 +32,36 @@ func GetTownCharactersPosition(id uint) []*model.CharacterPosition {
 	return positions
 }
 
+func getTownCharacterPosition(id uint, characterID uint) *model.CharacterPosition {
+	var position *model.CharacterPosition
+	r, err := config.RedisInstance().HGet(townUsersKey(id), strconv.FormatUint(uint64(characterID), 10)).Result()
+	if err != nil {
+		if err.Error() == "redis: nil" {
+			return nil
+		} else {
+			panic(err)
+		}
+	} else {
+		json.Unmarshal([]byte(r), &characterID)
+	}
+	return position
+}
+
+func setDefaultPosition(characterID uint) {
+	townID := uint(1)
+	town := FindTown(townID)
+
+	coordinate := map[string]uint{
+		"x": town.Position.X,
+		"y": town.Position.Y,
+	}
+
+	coordinateJson, _ := json.Marshal(coordinate)
+
+	err := config.RedisInstance().HSet(townUsersKey(townID), strconv.FormatUint(uint64(characterID), 10), coordinateJson).Err()
+	if err != nil { panic(err) }
+}
+
 func SetTownCharacterPosition(townID uint, position *model.CharacterPosition) {
 	coordinate := map[string]uint{
 		"x": position.X,
