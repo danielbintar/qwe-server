1d878a87ea9986b6d515b111a7e044625a16e3a1
@danielbintar <daniel_bintar@yahoo.com>
2019-02-28 07:35:11 +0700

    random position

--- app/game/monster.go
+++ app/game/monster.go
@@ -3,18 +3,22 @@ package game
 import (
 	// "fmt"
 	// "time"
+	"math/rand"
 	"github.com/danielbintar/qwe-server/repository"
 	"github.com/danielbintar/qwe-server/model"
 )
 
 func spawnMonster(id uint, monsterID uint, regionID uint, p model.RangePosition) model.MonsterSpawn {
+	x := uint(rand.Intn(int(p.MaxX - p.MinX))) + p.MinX
+	y := uint(rand.Intn(int(p.MaxY - p.MinY))) + p.MinY
+
 	spawn := model.MonsterSpawn {
 		ID: id,
 		MonsterID: monsterID,
 		RegionID: regionID,
 		Position: model.Position {
-			X: p.MinX,
-			Y: p.MinY,
+			X: x,
+			Y: y,
 		},
 	}
 
